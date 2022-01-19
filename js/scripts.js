92231a24c2ba2a198805efb9aa8f7976c1504e45
Adithya B <bmadithya@gmail.com>
2022-01-19 15:18:34 -0700

    Upgraded to TW5.2
    - added a new blog post

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  window.dataLayer = window.dataLayer || [];
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
+  function gtag() {
+    dataLayer.push(arguments);
   }
+  gtag('js', new Date());
 
-  var disqus_config = function () {
-    this.page.url = "<<get_absolute_url>>";
-    this.page.identifier = "<<get_page_uniqueid>>";
-  };
-  (function () {
-    var d = document, s = d.createElement('script');
-    s.src = 'https://adithyab-in.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
+  gtag('config', 'UA-171642977-1');
+
+  const e = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+  e.length > 0 && e.forEach(e => {
+    e.addEventListener("click", () => {
+      const t = e.dataset.target,
+        a = document.getElementById(t);
+      e.classList.toggle("is-active"), a.classList.toggle("is-active")
+    })
+  })
+
+  // var disqus_config = function () {
+  //   this.page.url = document.querySelector('meta[name="page_canonical_url"]').content;
+  //   this.page.identifier = document.querySelector('meta[name="page_unique_id"]').content;
+  // };
+  // (function () {
+  //   var d = document, s = d.createElement('script');
+  //   s.src = 'https://adithyab-in.disqus.com/embed.js';
+  //   s.setAttribute('data-timestamp', +new Date());
+  //   (d.head || d.body).appendChild(s);
+  // })();
 
 });
